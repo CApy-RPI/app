888e4e57dc45ac4f18f0dddbdd0ee872155316bb
zbrotherton <zanebrotherton@gmail.com>
2024-10-15 17:51:17 -0400

    Possible implementation of admin ping

--- cogs/ping.py
+++ cogs/ping.py
@@ -24,6 +24,24 @@ class Ping(commands.Cog):
         self.logger.info(message)
         await ctx.send(embed=embed)
 
+    @commands.command(name="Admin ping", help="ADMIN - Shows the bot's latency.")
+    @commands.has_permissions(administrator=True)
+    async def admin_ping(self, ctx):
+        message = f"⏱ {round(self.bot.latency * 1000)} ms Latency!"
+        embed = discord.Embed(
+            title="Ping",
+            description=message,
+            color=discord.Color.pink(),
+        )
+        self.logger.info(message)
+        await ctx.send(embed=embed)
+
+    @admin_ping.error
+    async def admin_ping_error(ctx, error):
+        if isinstance(error, commands.MissingPermissions):
+            msg = "Not an admin {}".format(ctx.message.author.mention)  
+            await ctx.send(msg)
+
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(Ping(bot))
